93c091db3fc02c0e8362f152715bb1004c59e42d
Rune Flobakk <rune.flobakk@posten.no>
2024-09-08 19:16:39 +0200

    Add adapter for java.net.URI

---
+++ bind-javax/src/test/java/no/posten/javax/xml/bind/adapter/UriXmlAdapterTest.java
@@ -0,0 +1,35 @@
+package no.posten.javax.xml.bind.adapter;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static uk.co.probablyfine.matchers.Java8Matchers.where;
+
+class UriXmlAdapterTest {
+
+    private final UriXmlAdapter adapter = new UriXmlAdapter();
+
+    @Test
+    void handlesNull() {
+        assertAll(
+                () -> assertThat(adapter.marshal(null), nullValue()),
+                () -> assertThat(adapter.unmarshal(null), nullValue()));
+    }
+
+    @Test
+    void convertsToUri() {
+        assertThat("https://posten.no", where(adapter::unmarshal, is(URI.create("https://posten.no"))));
+    }
+
+    @Test
+    void convertsToStringValue() {
+        assertThat(URI.create("https://posten.no"), where(adapter::marshal, is("https://posten.no")));
+        assertThat(URI.create("https://nårvægian.charactørs.no"), where(adapter::marshal, is("https://n%C3%A5rv%C3%A6gian.charact%C3%B8rs.no")));
+    }
+
+}
